effa3a662ee890689b60a5f755a080e7f8a1e1b3
Caio Silva Freitas <freitass_caio@icloud.com>
2025-08-16 10:58:50 -0300

    Product

---
+++ src/main/java/com/meuprimeiroprojeto/course/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.meuprimeiroprojeto.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.meuprimeiroprojeto.course.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
